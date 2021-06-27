8a99470d160c5cca9fe09789a82956c47f592444
harrymuluk <81852925+harrymuluk@users.noreply.github.com>
2021-06-27 19:24:05 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>HareSh apeAI</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Dev with Javascript and React.js</h1>
+      <p>
+        Hii This is Haresh Muluk Amazing Bootcamp is Taken Everything will be
+        understand Thank You!
+      </p>
+    </div>
+  );
+}
+
+export default Note;
